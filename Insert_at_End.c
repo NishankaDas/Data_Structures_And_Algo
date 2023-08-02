d8c1a906e693ddd4b2dc7c48ba50cd0a4e37183b
Nishanka Das <137529239+NishankaDas@users.noreply.github.com>
2023-08-03 03:33:43 +0530

    Create Insert_at_End.c

---
+++ Insert_at_End.c
@@ -0,0 +1,41 @@
+//Inserting node at last
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct a {
+    int data;
+    struct a *link;
+}node;  //building a struct node type variable wich can hold a int type data and an address
+node *head = NULL; //creating a pointer to hold the address of the node
+void create(int num)
+{
+    node *newnode = (node*)malloc(sizeof(node)); //creating a node
+    newnode->data = num; // updating value in the data part
+    newnode->link = NULL; // setting the link as null
+    if(head == NULL) // checking if head is NULL
+    {
+     head = newnode; // giving the address of the newly created node
+    }
+    else{
+        node *temp; // creating a node type pointer
+        temp= head; // assigning temp the value of head
+        while(temp->link !=NULL) //if temp->link is not null then there is more node in list
+        {
+            temp = temp->link; // getting the next from the present node and assigning the value of temp as it
+        }
+        temp->link = newnode; // if the link is null then its not connectec to the newly created node , si cinnecting the node
+    }
+}
+void see(node *x){
+while(x!=NULL) //checking if the value of x (means the given value or increased value) is null or not
+               // if it was x->link!=NULL the last node would not be check =ed and not be printed by the f(x)
+{
+    printf("%d\n",x->data); // printing dsata part
+    x = x->link; // increamenting the link part of teh node
+}
+}
+int main(){
+create(99);
+create(77);
+see(head);
+return 0;
+}
